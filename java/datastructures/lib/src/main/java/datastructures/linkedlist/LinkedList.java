e50cef1a83f9843a5ae9cdc685ace6584126cff0
blieberman@healthcareanalytics.expert <blieberman@healthcareanalytics.expert>
2022-10-28 18:24:56 -0600

    Linked List challenge complete

--- java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -2,9 +2,46 @@ package datastructures.linkedlist;
 
 public class LinkedList
 {
-  @Override
-  public String toString()
-    {
-        return "null";  // TODO: implement me
+  public Node head;
+
+  public LinkedList() {
+    this.head = null;
+  }
+
+  public LinkedList(Node head) {
+    this.head = head;
+  }
+
+  public LinkedList(Node head, Node next) {
+    this.head = head;
+    this.head.next = next;
+  }
+
+  public void insert(Node value) {
+    value.next = this.head;
+    this.head = value;
+  }
+
+  public Boolean includes(Node value) {
+    Node current = this.head;
+    while (current != null) {
+      if (current == value) return true;
+      else current = current.next;
     }
+    return false;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    Node current = this.head;
+    while (current != null) {
+      sb.append("{")
+        .append(current.getValue())
+        .append("}->");
+      current = current.next;
+    }
+    sb.append("NULL");
+    return sb.toString();
+  }
 }
